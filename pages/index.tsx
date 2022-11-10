d76517d122cd94eb79d719c5ecfa79d5b8b7385b
Farish Noorhisham <farishnoorhisham81@gmail.com>
2022-11-10 12:15:40 +0000

    remove hero post

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,7 @@ type Props = {
 }
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const morePosts = allPosts
   return (
     <>
       <Layout>
@@ -22,16 +21,6 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
