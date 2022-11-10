eaaf6332f823b911ed5ce3c48c4992c2817bddfc
Farish Noorhisham <farishnoorhisham81@gmail.com>
2022-11-10 12:26:47 +0000

    hi, it's me

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,10 +3,10 @@ import imageFile from "../public/assets/memoji.png"
 
 const Intro = () => {
   return (
-    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col flex items-center md:justify-between mt-10 mb-16 md:mb-12">
       <Image src={imageFile} alt="A memoji" className="mr-10" />
-      <h1 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8 text-center">
-        Hi, welcome to my personal blog/portfolio site.
+      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
+        Hi, it's me
       </h1>
     </section>
   )
