0ec31da0ea8e58a68f4c184b1a019b4bf9cdf9b6
Farish Noorhisham <farishnoorhisham81@gmail.com>
2022-11-10 12:44:16 +0000

    lower memoji size

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ import imageFile from "../public/assets/memoji.png"
 const Intro = () => {
   return (
     <section className="flex-col flex items-center md:justify-between mt-10 mb-16 md:mb-12">
-      <Image src={imageFile} alt="A memoji" className="mr-10" />
+      <Image src={imageFile} alt="A memoji" className="w-48 h-48" />
       <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
         Hi, it's me
       </h1>
