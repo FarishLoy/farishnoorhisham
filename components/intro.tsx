ec4cb2feca7d4e415e8e2455fb9a26bc8e5b7135
Farish Noorhisham <farishnoorhisham81@gmail.com>
2022-12-11 17:39:44 +0000

    change intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,9 +5,11 @@ const Intro = () => {
   return (
     <section className="flex-col flex items-center md:justify-between mt-10 mb-16 md:mb-12">
       <Image src={imageFile} alt="A memoji" className="w-48 h-48" />
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
-        Hi, it's me
-      </h1>
+      <div>
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
+          It's me, hi
+        </h1>
+      </div>
     </section>
   )
 }
